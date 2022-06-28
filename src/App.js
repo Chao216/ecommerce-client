e9a4c6e3ae665530a99be3c43183dd364e1b7c62
Chao216 <co.jiaan@proton.me>
2022-06-28 17:19:45 +0300

    regular push from shell script

--- src/App.js
+++ src/App.js
@@ -7,15 +7,26 @@ function App() {
       </header>
       <main>
         <h1>Featured products</h1>
-        {data.map((item, idx) => {
-          return (
-            <div key={idx}>
-              <img src={item.image} alt={item.name} />
-              <p>{item.name}</p>
-              <p>{item.price}</p>
-            </div>
-          );
-        })}
+        <div className="products">
+          {data.map((item, idx) => {
+            return (
+              <div key={idx} className="product">
+                <a href={`/product/${item.slug}`}>
+                  <img src={item.image} alt={item.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${item.slug}`}>
+                    <p>{item.name}</p>
+                  </a>
+                  <p>
+                    <b>{item.price}</b>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
