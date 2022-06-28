e73aeb01fcdc68e39007e5684f12707adba7e8ec
Chao216 <co.jiaan@proton.me>
2022-06-28 16:49:55 +0300

    regular push from shell script

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # the client side rendered by React.js
+
+if you see such a warning from browser console `Warning: Each child in a list should have a unique "key" prop.`
+simply add a second param to your map() method, as use index as key
+
+```javascript
+{
+  data.map((item, idx) => {
+    return (
+      <div key={idx}>
+        <img src={item.image} alt={item.name} />
+        <p>{item.name}</p>
+        <p>{item.price}</p>
+      </div>
+    );
+  });
+}
+```
--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { data } from "./data";
 function App() {
   return (
     <div>
       <header>
         <a href="/">shoppin</a>
       </header>
+      <main>
+        <h1>Featured products</h1>
+        {data.map((item, idx) => {
+          return (
+            <div key={idx}>
+              <img src={item.image} alt={item.name} />
+              <p>{item.name}</p>
+              <p>{item.price}</p>
+            </div>
+          );
+        })}
+      </main>
     </div>
   );
 }
