1f69bc5c3f786e3f2bc35c1a1bb1c56d4a55731d
Chao216 <co.jiaan@proton.me>
2022-06-30 18:10:57 +0300

    use bootstrap for decoration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import About from "./screens/About";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import NavbarBrand from "react-bootstrap/esm/NavbarBrand";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">shoppin</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <NavbarBrand>shoppin</NavbarBrand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
